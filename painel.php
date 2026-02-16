dac8bd291fc2ac2d3257c1ad84b83e065d026016
edutvw <danielbarbosaeduardo@gmail.com>
2026-02-16 18:46:06 -0300

    primeira aula

---
+++ painel.php
@@ -0,0 +1,15 @@
+<?php
+/*imprimindo a variavel primeira vez */
+$idade = 30;
+
+echo ($idade);
+
+echo "<br>";
+
+//Primeiro array
+$cadastro = array(
+     'cliente1' => 'Eduardo'
+); 
+
+echo $cadastro['cliente1'];
+?>
